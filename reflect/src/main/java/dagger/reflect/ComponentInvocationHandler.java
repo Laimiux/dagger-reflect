37e70ca3fca4cbd5fda9308068a7893a5825c474
Laimonas Turauskas <laimiux@gmail.com>
2019-07-19 10:16:03 -0700

    Handle component interfaces with generic types.

--- reflect/src/main/java/dagger/reflect/ComponentInvocationHandler.java
+++ reflect/src/main/java/dagger/reflect/ComponentInvocationHandler.java
@@ -21,6 +21,7 @@ import static dagger.reflect.Reflection.newProxy;
 import dagger.MembersInjector;
 import dagger.Subcomponent;
 import dagger.reflect.Binding.LinkedBinding;
+
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
@@ -34,14 +35,16 @@ final class ComponentInvocationHandler implements InvocationHandler {
   }
 
   static <C> C create(Class<C> cls, Scope scope) {
-    return newProxy(cls, new ComponentInvocationHandler(scope));
+    return newProxy(cls, new ComponentInvocationHandler(cls, scope));
   }
 
+  private final Class<?> rootClass;
   private final Scope scope;
   private final ConcurrentHashMap<Method, MethodInvocationHandler> handlers =
       new ConcurrentHashMap<>();
 
-  private ComponentInvocationHandler(Scope scope) {
+  private ComponentInvocationHandler(Class<?> rootClass, Scope scope) {
+    this.rootClass = rootClass;
     this.scope = scope;
   }
 
@@ -53,7 +56,7 @@ final class ComponentInvocationHandler implements InvocationHandler {
 
     MethodInvocationHandler handler = handlers.get(method);
     if (handler == null) {
-      handler = createMethodInvocationHandler(method, scope);
+      handler = createMethodInvocationHandler(rootClass, method, scope);
       MethodInvocationHandler replaced = handlers.putIfAbsent(method, handler);
       if (replaced != null) {
         handler = replaced;
@@ -63,7 +66,7 @@ final class ComponentInvocationHandler implements InvocationHandler {
   }
 
   private static ComponentInvocationHandler.MethodInvocationHandler createMethodInvocationHandler(
-      Method method, Scope scope) {
+      Class<?> rootClass, Method method, Scope scope) {
     Type returnType = method.getGenericReturnType();
     Class<?>[] parameterTypes = method.getParameterTypes();
 
@@ -87,7 +90,7 @@ final class ComponentInvocationHandler implements InvocationHandler {
     }
 
     if (parameterTypes.length == 0) {
-      Key key = Key.of(findQualifier(method.getDeclaredAnnotations()), returnType);
+      Key key = Key.of(findQualifier(method.getDeclaredAnnotations()), TypeUtil.resolveType(rootClass, returnType));
       LinkedBinding<?> binding = scope.getBinding(key);
       return new ProvisionMethodInvocationHandler(binding);
     }
